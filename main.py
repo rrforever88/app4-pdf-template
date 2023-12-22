16738cf40e860109f31ea8950da20b4c8aaddd09
christophernelson <irish.claret@gmail.com>
2023-12-22 10:31:23 -0600

    Day 24: Add lines

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fpdf import FPDF
 import pandas as pd
 
+# Create a pdf object
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
-df = pd.read_csv('topics.csv')
+df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -13,7 +14,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    for y in range(20, 298, 10):
+        pdf.line(x1=10, y1=y, x2=200, y2=y)
 
     # Set the footer
     pdf.ln(265)
@@ -23,6 +26,10 @@ for index, row in df.iterrows():
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+        for y in range(20, 298, 10):
+            pdf.line(x1=10, y1=y, x2=200, y2=y)
+
+
 
         # Set the footer
         pdf.ln(277)
@@ -30,5 +37,4 @@ for index, row in df.iterrows():
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
 
-
-pdf.output("output.pdf")
+pdf.output('output.pdf')
